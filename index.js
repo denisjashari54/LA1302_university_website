478053421912e29678acdc735453f493a54dcfee
Densi Jashari <107101355+denisjashari54@users.noreply.github.com>
2022-12-11 14:11:56 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+
+    if(localStorage.getItem('students') != null){
+        var array = JSON.parse(localStorage.getItem('students'));
+        $('#studentsCount').text(array.length);
+    }
+
+
+})
